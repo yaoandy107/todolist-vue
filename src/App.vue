64cc920951be5849b89efa67007fafd69357c5ef
yaoandy107 <yaoandy107@gmail.com>
2018-09-06 15:54:43 +0800

    Add new todo function

--- src/App.vue
+++ src/App.vue
@@ -14,18 +14,30 @@
             <v-toolbar card>
               <v-toolbar-title class="body-2 grey--text">待辦清單</v-toolbar-title>
               <v-spacer></v-spacer>
+              <!-- 搜尋按鈕 -->
               <v-btn icon>
                 <v-icon>search</v-icon>
-              </v-btn>
-              <v-btn icon>
-                <v-icon>add</v-icon>
               </v-btn>
             </v-toolbar>
 
             <v-divider></v-divider>
             
             <!-- 待辦清單列表 -->
-            <v-card-text style="min-height: 200px;"></v-card-text>
+            <v-layout column>
+              <todo-item 
+                v-for="(item, index) of todoList"
+                :key="index"
+                :content="item">
+              </todo-item>
+            </v-layout>
+            <v-text-field 
+              v-model="inputTodo"
+              label="Outline"
+              box
+              :append-icon="inputTodo ? 'send' : ''"
+              @click:append="addTodo"
+              @keyup.enter="addTodo">
+            </v-text-field>
           </v-card>
         </v-flex>
       </v-layout>
@@ -33,8 +45,25 @@
 </template>
 
 <script>
+import TodoItem from './components/TodoItem.vue'
 export default {
+  components: {
+    'todo-item': TodoItem
+  },
+  data () {
+    return {
+      inputTodo: '',
+      todoList: [
 
+      ]
+    }
+  },
+  methods: {
+    addTodo () {
+      this.todoList.push(this.inputTodo)
+      this.inputTodo = ''
+    }
+  }
 }
 </script>
 
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <li>{{content}}</li>
+</template>
+
+<script>
+export default {
+  props: [
+    'content'
+  ]
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
