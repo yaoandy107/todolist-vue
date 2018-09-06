4c23d28798b052680e03531d043b4a87ddc50d04
yaoandy107 <yaoandy107@gmail.com>
2018-09-06 15:25:00 +0800

    Add todolist layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <v-toolbar
+        color="primary"
+        dark
+        extended>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+      </v-toolbar>
+      <!-- 待辦清單 -->
+      <v-layout wrap>
+        <v-flex xs8 offset-xs2>
+          <v-card class="card--flex-toolbar">
+            <!-- 待辦清單標題 -->
+            <v-toolbar card>
+              <v-toolbar-title class="body-2 grey--text">待辦清單</v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-btn icon>
+                <v-icon>search</v-icon>
+              </v-btn>
+              <v-btn icon>
+                <v-icon>add</v-icon>
+              </v-btn>
+            </v-toolbar>
+
+            <v-divider></v-divider>
+            
+            <!-- 待辦清單列表 -->
+            <v-card-text style="min-height: 200px;"></v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .card--flex-toolbar {
+    margin-top: -64px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App)
 }).$mount('#app')
