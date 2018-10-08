84bc31ae39402585f0a99a1b8533f0e09aa3d47e
yaoandy107 <yaoandy107@gmail.com>
2018-10-08 11:26:33 +0800

    Change the font size of timestamp

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -2,7 +2,7 @@
   <v-layout row class="mx-4 my-2">
     <div class="body-1 ma-auto">{{todo.content}}</div>
     <v-spacer></v-spacer>
-    <div v-if="todo.timestamp" class="ma-auto mr-3">時間：{{todo.timestamp.toDate().toLocaleString()}}</div>
+    <div v-if="todo.timestamp" class="ma-auto caption">時間：{{todo.timestamp.toDate().toLocaleString()}}</div>
     <v-btn 
       icon
       @click="handleDelete">
